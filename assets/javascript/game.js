7be27f78f350a418a660cb21c5eeb97df5c7ba3f
Robidobi33 <robidobi33@gmail.com>
2018-05-19 19:21:01 -0600

    update_02

---
+++ assets/javascript/game.js
@@ -0,0 +1,62 @@
+var quiz = [
+
+    qOne = {
+        q: "Who isnt part of the Tribunal?",
+        a:  "Neravar",
+        b:  "Vivic",
+        c:  "Alemexia",
+        d:  "Sotha Sil",
+        ca: "Neravar"
+    },
+
+    qTwo = {
+        q: "Which daedric prince is known as the Mad God?",
+        a: "Molag Bal",
+        b: "Sheograth",
+        c: "Azura",
+        d: "Boethiah",
+        ca: "Sheograth"
+    },
+
+    qThree = {
+        q: "Who were the ancestors of the Dunmer?",
+        a: "Altmer",
+        b: "Ayleid",
+        c: "Chimer",
+        d: "Maormer",
+        ca: "Chimer"
+    },
+
+    qFour = {
+        q: "What is the name of Morrowinds secret society of assasins?",
+        a: "Dark Brotherhood",
+        b: "Comonna Tong",
+        c: "Morag Tong",
+        d: "Shadow Scales",
+        ca: "Morag Tong"
+    },
+
+    qFive = {
+        q: "Which race was one of two enslaved races by the Dunmer?",
+        a:  "Khajiit",
+        b:  "Bosmer",
+        c:  "Falmer",
+        d:  "Orc",
+        ca: "Khajiit"
+    }
+
+];
+
+function startQuiz(){
+
+    
+
+}
+
+/*for(i=0;i<quiz.length;i++){
+    $("Question").html(quiz[i].q);
+    $("aOne").html(quiz[i].a);
+    $("aTwo").html(quiz[i].b);
+    $("aThree").html(quiz[i].c);
+    $("aFour").html(quiz[i].d);
+};*/
